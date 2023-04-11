32cc5896828f4873ebc4408b30a7f05cf5fe175a
luka mkalavishvili <mkalavishvililuka00@gmail.com>
2023-04-11 13:46:39 +0400

    prep

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+    <button @click="onClick()" :style="{background: color}" :class="`btn ${color}`">
+        {{ text }}
+    </button>
+</template>
+
+
+<script>
+
+    export default{
+        name: 'Button',
+        props: {
+            text: {
+                default: 'Add Task'
+            },
+            color:{
+                type: String,
+                default: 'green'
+            }
+        },
+        methods:{
+            onClick(){
+                console.log('click');
+            }
+        }
+    }
+
+</script>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,31 @@
+<template>
+    <div :key="task.id" v-for="task in tasks">
+        <Task @delete-task="$emit('delete-task', task.id)" @toggle-reminder="$emit('toggle-reminder', task.id)" :task="task"/>
+    </div>
+</template>
+
+
+<script>
+
+    import Task from './Task.vue';
+
+    export default {
+        name: 'Tasks',
+        props: {
+            tasks: Array
+        },
+        components: {
+            Task
+        },
+        methods:{
+			toggleReminder(id){
+				console.log(id);
+			},
+        },
+        emits:[
+            'delete-task',
+            'toggle-reminder'
+        ]
+    }
+
+</script>
